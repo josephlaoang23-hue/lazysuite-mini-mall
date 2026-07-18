c3c12e5ab8bb6c2c2252432774aafdb3466d5cb4
josephlaoang23-hue <josephlaoang23@gmail.com>
2026-07-18 22:23:55 +0800

    update ads

--- src/ads/adConfig.ts
+++ src/ads/adConfig.ts
@@ -1,5 +1,5 @@
 export const ADSTERRA_SKYSCRAPER_KEY = 'c016e00c8ecbd9996f643ce468ee30c0';
-export const ADSTERRA_SKYSCRAPER_SRC = 'https://highperformanceformat.com';
+export const ADSTERRA_SKYSCRAPER_SRC = 'https://highperformanceformat.com/...'; // see note below
 
-export const POPUNDER_URL = 'https://effectivecpmnetwork.com';
-export const SOCIAL_BAR_URL = 'https://effectivecpmnetwork.com';
+export const POPUNDER_URL = 'https://pl30421285.effectivecpmnetwork.com/09/b4/19/09b41998a581a38786ed96b34a6ae600.js';
+export const SOCIAL_BAR_URL = 'https://pl30421283.effectivecpmnetwork.com/13/7e/04/137e04df861d66d72cbe43f43866befb.js';
